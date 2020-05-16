c057a17746e3c52511e97fffe54c036871bba845
zhujun <Yaolan@Lans-MacBook.local>
2020-05-15 22:47:51 -0700

    add simple server code

---
+++ src/main/java/com/github/zhu/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,26 @@
+package com.github.zhu.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received Shut down request");
+            server.shutdown();
+            System.out.println("Successfully stop the server");
+        }));
+        
+        server.awaitTermination();
+    }
+}
